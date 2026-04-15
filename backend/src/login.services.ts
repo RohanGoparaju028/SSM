d9fc58c4ce01aa159b7d7e4a056f16c22f4fa41a
Rohan Goparaju <goparajurohan41@gmail.com>
2026-04-15 14:20:30 -0400

    Started to work on login

---
+++ backend/src/login.services.ts
@@ -0,0 +1,31 @@
+import { Injectable, InternalServerErrorException, BadRequestException, HttpException } from '@nestjs/common'
+import { PrismaService } from './prisma.service.js';
+import * as  bcrypt from 'bcrypt';
+@Injectable()
+export class LoginServices {
+    constructor(private prisma: PrismaService) { }
+
+    async Login(email: string, password: string): Promise<{ message: string, statuscode: number }> {
+        try {
+            const user = await this.prisma.user.findUnique({
+                where: { Email: email },
+            });
+
+
+            if (!user) {
+                throw new BadRequestException("Cannot find the email in our database please check again");
+            }
+            let storedPassword = await bcrypt.compare(password, user.Password);
+            if (storedPassword) {
+                return { message: "User exist", statuscode: 302 };
+            } else {
+                return { message: "Wrong Password", statuscode: 401 };
+            }
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            }
+            throw new InternalServerErrorException("Cannot verify the login at this moement");
+        }
+    }
+}
